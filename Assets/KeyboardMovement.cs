187249eeabb5a62c53584a404781e1636b9e6bed
Tianrui Zhang <tz85@duke.edu>
2020-02-02 16:02:03 -0500

    Added movement script for testing streaming

---
+++ Assets/KeyboardMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    public float movementSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        int horizontalInput = 0;
+        //get the Input from Vertical axis
+        int verticalInput = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            horizontalInput += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            horizontalInput -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            verticalInput += 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            verticalInput -= 1;
+        }
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);
+
+        //output to log the position change
+        Debug.Log(transform.position);
+    }
+}
